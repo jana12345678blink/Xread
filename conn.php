17423985f1b8cbf65f50c87111689302e1d35d8f
jana12345678blink <160288060+jana12345678blink@users.noreply.github.com>
2026-02-27 22:53:11 +0200

    Delete conn.php

--- conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root"; // Change this to your database username
-$password = ""; // Change this to your database password
-$dbname = "CoverToCover";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
